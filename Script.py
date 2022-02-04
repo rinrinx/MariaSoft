42189ee871b6dd5da856c637cfed9d6477496d3a
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-05 02:00:48 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,7 +6,7 @@ STATUS_TXT = """
 ★ Topladım kullanıcı: <code>{}</code>
 ★ Kullanılmış depolama: <code>{}</code> 𝙼𝚒𝙱
 ★ Ücretsiz depolama: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_P = """#YeniKullanici
+LOG_TEXT_P = """#YeniKullanici
 ID - <code>{}</code>
 Ad - {}
 """
