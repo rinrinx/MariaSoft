8f987f2833d36ad774d6475433fe4842137740c9
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-05 22:56:03 +0300

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -18,21 +18,3 @@ async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
     await message.reply(f'Üzgünüm dostum, beni kullanman yasaklandı. \nSebep: {ban["ban_reason"]}')
 
-
-@Client.on_message(filters.group & filters.incoming)
-async def grp_bd(bot, message):
-    buttons = [[
-        InlineKeyboardButton('Korsan', url=f'https://t.me/{SUPPORT_CHAT}')
-    ]]
-    reply_markup=InlineKeyboardMarkup(buttons)
-    k = await message.reply(
-        text=f"**Ben gruplara kapalıyım dostum.**",
-        reply_markup=reply_markup,
-        parse_mode='markdown',
-    )
-    try:
-        await k.pin()
-        await bot.leave_chat(message.chat.id)
-    except:
-        pass
-    await bot.leave_chat(message.chat.id)
